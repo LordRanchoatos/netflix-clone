ca29330ed0333638c069e6e41e2771c5018ad39c
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-05-06 03:06:20 +0100

    header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
       <Head>
         <title>Netflex</title>
         <link rel="icon" href="/favicon.ico" />
