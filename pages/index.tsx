474800864c507ec241b128bc08e5cb1a625ab25b
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-05-08 03:43:07 +0100

    fix bloat: blend bg banner with section

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const Home = ({
   trendingNow,
 } : Props) => {
   return (
-    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
+    <div className='relative h-screen bg-gradient-to-b from-gray-900/10  lg:h-[140vh]'>
       <Head>
         <title>Home - Netflex</title>
         <link rel="icon" href="/favicon.ico" />
