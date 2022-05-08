c4df14a575a83b004a349edba8a8795e731bc132
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-05-08 04:11:48 +0100

    fix title issue

--- components/Row.tsx
+++ components/Row.tsx
@@ -35,9 +35,9 @@ function Row({title, movies}: Props) {
      
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
-        <h2 className="h-40 cursor-pointer 
+        <h2 className="w-56 cursor-pointer 
             text-sm font-semibold text-[#e5e5e5] 
-            transition-duration-200 hover:text-white">
+            transition duration-200 hover:text-white">
                 {title}
         </h2>
         <div className="group relative md:ml-2">
